21422313d6262007b88e5de22df7b6cab8173b20
henriits <121551949+henriits@users.noreply.github.com>
2023-01-30 15:20:05 +0200

    Created columns, image, and description

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png", width=300,)
+
+
+with col2:
+    st.title("Henri Tsarents")
+    content = """
+    Hi , I am Henri. 
+    
+    I am a junior Python programmer, currently learning how to make 
+    web apps with python. Also learning web development. 
+    
+    I started my journey as a programmer at 2022 september 
+    in Software Development Academy.
+    Simultaneously pushing my way through numerous Udemy boot camps.
+    
+    
+    """
+    st.info(content)
