ef98cd87213240bb095314215c873b505a26324e
henriits <121551949+henriits@users.noreply.github.com>
2023-01-30 20:16:56 +0200

    sending email using smtplib

--- send_email.py
+++ send_email.py
@@ -1,21 +1,21 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "henritsarents@gmail.com"
-password = "dzkelmvqamwfodng"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "henritsarents@gmail.com"
+    password = "dzkelmvqamwfodng"
+    receiver = "henritsarents@gmail.com"
+    context = ssl.create_default_context()
+    # message = """Subject:
+    #             Hi!
+    #             How are you?
+    #             Bye!"""
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-receiver = "henritsarents@gmail.com"
 
-context = ssl.create_default_context()
 
-message = """Subject: 
-Hi!
-How are you?
-Bye!"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
